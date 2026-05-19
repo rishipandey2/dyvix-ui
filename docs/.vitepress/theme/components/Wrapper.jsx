053861db2b49655d86bee8a03f7a717a5d20e095
younisdev <younisayoub10@gmail.com>
2026-05-19 03:08:34 +0300

    Polish format and design of dyvix docs playground

--- docs/.vitepress/theme/components/Wrapper.jsx
+++ docs/.vitepress/theme/components/Wrapper.jsx
@@ -28,9 +28,9 @@ export default function Wrapper({
       (ele) => ele.utility === activeConfig && ele.type === 'config'
     );
 
-    const currentData = { 
-      ...currentConfig, 
-      amount: currentConfig.amount || amount 
+    const currentData = {
+      ...currentConfig,
+      amount: currentConfig.amount || amount
     };
 
     const isvalid = { status: 1, message: null };
@@ -78,18 +78,17 @@ export default function Wrapper({
     setActive('');
   }
 
-  function delconfig(index)
-  {
+  function delconfig(index) {
     componentCallback((prev) =>
-    prev.map((item) =>
-      item.utility === activeConfig
-        ? { 
-            ...item, 
-            current: item.current.filter((_, i) => i !== index) 
-          }
-        : item
-    )
-  );
+      prev.map((item) =>
+        item.utility === activeConfig
+          ? {
+              ...item,
+              current: item.current.filter((_, i) => i !== index)
+            }
+          : item
+      )
+    );
   }
 
   React.useEffect(() => {
@@ -222,7 +221,7 @@ export default function Wrapper({
                             );
                           })}
                         </div>
-                        <div className='config-toolbar'>
+                        <div className="config-toolbar">
                           <button
                             className="playground-config-btn"
                             onClick={() => setActive('')}
@@ -231,139 +230,150 @@ export default function Wrapper({
                           </button>
                           <button
                             className="playground-config-btn"
-                            onClick={() => SetAdd((prev => !prev))}
+                            onClick={() => SetAdd((prev) => !prev)}
                           >
                             +
                           </button>
                         </div>
-                        {showAdd &&
-                        <div className='playground-add-config'>
-                        {ele['config-rules'].map((rule) => {
-                          const format = rule.formats;
-                          const hasMultiple = format.includes('multiple');
-                          const hasOptions = rule.options;
-                          const isUnique = format.find((e) => e === 'unique');
-                          const isAmount = ele['config-rules'].find(
-                            (ele) => ele.amount_field === true
-                          );
-                          const type = format.find(
-                            (e) =>
-                              e === 'string' || e === 'number' || e === 'array'
-                          );
+                        {showAdd && (
+                          <div className="playground-add-config">
+                            {ele['config-rules'].map((rule) => {
+                              const format = rule.formats;
+                              const hasMultiple = format.includes('multiple');
+                              const hasOptions = rule.options;
+                              const isUnique = format.find(
+                                (e) => e === 'unique'
+                              );
+                              const isAmount = ele['config-rules'].find(
+                                (ele) => ele.amount_field === true
+                              );
+                              const type = format.find(
+                                (e) =>
+                                  e === 'string' ||
+                                  e === 'number' ||
+                                  e === 'array'
+                              );
 
-                          return (
-                            <div key={rule.rule} className="config-field-group">
-                              <label>{rule.rule}</label>
-                              {type === 'number' ? (
-                                <input
-                                  type="number"
-                                  placeholder={rule.rule}
-                                  max={rule.max_val}
-                                  min={1}
-                                  className="playground-text"
-                                  defaultValue={amount}
-                                  onChange={(e) => {
-                                    const val = Math.min(
-                                      Number(e.target.value),
-                                      rule.max_val
-                                    );
-                                    const clamped = Math.max(val, 1);
-                                    setAmount(clamped);
-                                    setConfig((prev) => ({
-                                      ...prev,
-                                      [rule.rule]: clamped
-                                    }));
-                                  }}
-                                  
-                                ></input>
-                              ) : hasOptions ? (
-                                <div className="dynamic-input-list">
-                                  {[
-                                    ...Array(hasMultiple ? Number(amount) : 1)
-                                  ].map((_, index) => (
-                                    <select
-                                      key={index}
-                                      className="playground-select"
-                                      onChange={(e) => {
-                                        const targetVal = e.target.value;
-                                        let currentVal;
-                                        setConfig((prev) => {
-                                          if (hasMultiple) {
-                                            currentVal = prev[rule.rule]
-                                              ? prev[rule.rule]
-                                              : [];
-                                            currentVal[index] = targetVal;
-                                          } else {
-                                            currentVal = targetVal;
-                                          }
-
-                                          return {
+                              return (
+                                <div
+                                  key={rule.rule}
+                                  className="config-field-group"
+                                >
+                                  <label>{rule.rule}</label>
+                                  {type === 'number' ? (
+                                    <div className="dynamic-input-list">
+                                      <input
+                                        type="number"
+                                        placeholder={rule.rule}
+                                        max={rule.max_val}
+                                        min={1}
+                                        className="playground-text"
+                                        defaultValue={amount}
+                                        onChange={(e) => {
+                                          const val = Math.min(
+                                            Number(e.target.value),
+                                            rule.max_val
+                                          );
+                                          const clamped = Math.max(val, 1);
+                                          setAmount(clamped);
+                                          setConfig((prev) => ({
                                             ...prev,
-                                            [rule.rule]: currentVal
-                                          };
-                                        });
-                                      }}
-                                    >
-                                      <option>None</option>
-                                      {Object.entries(hasOptions).map(
-                                        ([key, value]) => (
-                                          <option key={key} value={value}>
-                                            {key}
-                                          </option>
+                                            [rule.rule]: clamped
+                                          }));
+                                        }}
+                                      ></input>
+                                    </div>
+                                  ) : hasOptions ? (
+                                    <div className="dynamic-input-list">
+                                      {[
+                                        ...Array(
+                                          hasMultiple ? Number(amount) : 1
                                         )
-                                      )}
-                                    </select>
-                                  ))}
+                                      ].map((_, index) => (
+                                        <select
+                                          key={index}
+                                          className="playground-select"
+                                          onChange={(e) => {
+                                            const targetVal = e.target.value;
+                                            let currentVal;
+                                            setConfig((prev) => {
+                                              if (hasMultiple) {
+                                                currentVal = prev[rule.rule]
+                                                  ? prev[rule.rule]
+                                                  : [];
+                                                currentVal[index] = targetVal;
+                                              } else {
+                                                currentVal = targetVal;
+                                              }
+
+                                              return {
+                                                ...prev,
+                                                [rule.rule]: currentVal
+                                              };
+                                            });
+                                          }}
+                                        >
+                                          <option>None</option>
+                                          {Object.entries(hasOptions).map(
+                                            ([key, value]) => (
+                                              <option key={key} value={value}>
+                                                {key}
+                                              </option>
+                                            )
+                                          )}
+                                        </select>
+                                      ))}
+                                    </div>
+                                  ) : type === 'string' ? (
+                                    <div className="dynamic-input-list">
+                                      {[
+                                        ...Array(
+                                          hasMultiple ? Number(amount) : 1
+                                        )
+                                      ].map((_, index) => (
+                                        <input
+                                          key={index}
+                                          type="text"
+                                          placeholder={`${rule.rule} ${index + 1}`}
+                                          className="playground-text"
+                                          onChange={(e) => {
+                                            const targetVal = e.target.value;
+                                            let currentVal;
+                                            setConfig((prev) => {
+                                              if (hasMultiple) {
+                                                currentVal = prev[rule.rule]
+                                                  ? prev[rule.rule]
+                                                  : [];
+                                                currentVal[index] = targetVal;
+                                              } else {
+                                                currentVal = targetVal;
+                                              }
+                                              return {
+                                                ...prev,
+                                                [rule.rule]: currentVal
+                                              };
+                                            });
+                                          }}
+                                        />
+                                      ))}
+                                    </div>
+                                  ) : (
+                                    <></>
+                                  )}
                                 </div>
-                              ) : type === 'string' ? (
-                                <div className="dynamic-input-list">
-                                  {[
-                                    ...Array(hasMultiple ? Number(amount) : 1)
-                                  ].map((_, index) => (
-                                    <input
-                                      key={index}
-                                      type="text"
-                                      placeholder={`${rule.rule} ${index + 1}`}
-                                      className="playground-text"
-                                      onChange={(e) => {
-                                        const targetVal = e.target.value;
-                                        let currentVal;
-                                        setConfig((prev) => {
-                                          if (hasMultiple) {
-                                            currentVal = prev[rule.rule]
-                                              ? prev[rule.rule]
-                                              : [];
-                                            currentVal[index] = targetVal;
-                                          } else {
-                                            currentVal = targetVal;
-                                          }
-                                          return {
-                                            ...prev,
-                                            [rule.rule]: currentVal
-                                          };
-                                        });
-                                      }}
-                                    />
-                                  ))}
-                                </div>
-                              ) : (
-                                <></>
-                              )}
-                            </div>
-                          );
-                        })}
-                        <button
-                          className="playground-config-btn"
-                          onClick={() => handleConfigAddition()}
-                        >
-                          Add
-                        </button>
-                        </div>
-            }
+                              );
+                            })}
+                            <button
+                              className="playground-config-btn addbtn"
+                              onClick={() => handleConfigAddition()}
+                            >
+                              Add
+                            </button>
+                          </div>
+                        )}
                       </div>,
                       document.querySelector('.dyvix-playground-wrapper')
                     )}
-                    
                 </>
               );
             }
